0e7401c046744f64884e3b24252634385a72590d
Cuong Tran <99084745+cuongtrans@users.noreply.github.com>
2022-02-14 22:53:15 +0700

    Delete basic-join directory

--- basic-join/african-cities.sql
+++
@@ -1,6 +0,0 @@
--- Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
-SELECT CI.NAME
-FROM CITY AS CI
-LEFT JOIN COUNTRY AS CO
-ON CO.CODE = CI.COUNTRYCODE
-WHERE CO.CONTINENT = 'African';
