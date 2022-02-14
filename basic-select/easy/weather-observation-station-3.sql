8b93f39f18b273a0f96fe4921d792e80846d58f6
Cuong Tran <99084745+cuongtrans@users.noreply.github.com>
2022-02-14 13:35:50 +0700

    Create weather-observation-station-3.sql

---
+++ basic-select/easy/weather-observation-station-3.sql
@@ -0,0 +1,5 @@
+-- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+SELECT DISTINCT CITY
+FROM STATION ST 
+WHERE MOD(ST.ID,2) = 0
+ORDER BY CITY
