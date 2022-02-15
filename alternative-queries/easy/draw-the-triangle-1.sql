161245d34ed0c8bbd85f81cf37445f18d338651a
Cuong Tran <99084745+cuongtrans@users.noreply.github.com>
2022-02-15 17:15:34 +0700

    Update draw-the-triangle-1.sql

--- alternative-queries/easy/draw-the-triangle-1.sql
+++ alternative-queries/easy/draw-the-triangle-1.sql
@@ -1,5 +1,5 @@
 -- Write a query to print the pattern P(20).
 SET @number = 21;
-SELECT REPEAT('* ', @number := @number-1) 
+SELECT REPEAT('* ', @number := @number - 1) 
 FROM INFORMATION_SCHEMA.TABLES
 LIMIT 20;
